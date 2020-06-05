261b82dc02e3d19e6fdcc2554fcf7a7737c64150
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2020-06-05 19:04:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# Berm
+
+Berm is a simple library that helps to manage flag.
+
+The name come from **b**inary p**erm**missions because it can easily manage permissions using byte operations.
+
+### Rationale
+
+bit operations are quite simple (and thus fast), and it's sometimes useful to have permission system that can be used programmatically, think about showing or hiding certain ui features based on the context. So the goal of this tool is to have a good permission system with the least hoverhead as possbile while being easy to use.
+
+## Usage
+
+The class `Berm::Flag` represent the permission manager.
+
+It needs to know which flags it will have so you can use the `Berm::FlagsConstant("Myvar")` annotation or declaring the `FlagValues` variable. Only string and symbol array are supported.
+```crystal
+require "berm"
+
+class MyFlag < Berm::Flag
+  FlagValues = %i(Read Write)
+
+  # Foreach flag value (aka permission) a constant is generated
+  # Flags can be combined using the or operator
+  ReadWrite = Write | Read
+end
+
+# For every permission a method `permits_{permission}?` is generated
+puts MyFlag::Read.permits_read? # -> true
+puts MyFlag::Read.permits_read? # -> false
+
+puts MyFlag::ReadWrite.permits_read? # -> true
+puts MyFlag::ReadWrite.permits_write? # -> true
+
+# Also a None constant is generated to match the state of no permissions at all
+# The permits_something? tells wheter there's at least one permission enabled
+# it can be called on every permission
+puts MyFlag::None.permits_something? # -> false
+```
+
+This is the basic usage, you can find more usage examples in `examples`.
+
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/berm/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Giuseppe Pagano](https://github.com/your-github-user) - creator and maintainer
---
+++ src/berm.cr
@@ -0,0 +1,104 @@
+module Berm
+  VERSION = "0.1.0"
+
+  # This annotations it used to tell Berm in which variable flag values are stored
+  annotation FlagsConstant
+  end
+
+  # Flag is the permission manager.
+  #
+  # It loads permission values from the variable pointed out by the `FlagsConstant` annotation,
+  # otherwise from FlagValues and raises a compilation error if there's none of the above.
+  # This variable has to be an array of symbols or strings, starting with the capital letter.
+  #
+  # It stores the values in The enum `PermValues` using powers of two as values, which make
+  # easy to combine multiple permissions.
+  #
+  # For each permission value happens the following:
+  #   - the value is added to PermValues
+  #   - the representation of the value as a `Flag` object is saved as class constant using the same name
+  #   - a method permits_{value}? is generated telling if that permission is enabled NOTE: value, here, is converted in snake case.
+  #
+  # A None constant with `PermValues::None` is generated to be the zero-value.
+  # A permits_something? method is added to tell if there's at least one permission enabled
+  class Flag
+    macro init_values
+      @[Flags]
+      enum PermValues
+        None = 0
+        {% if @type.has_constant?("FLAGVALUES") %}
+          {% for perm in @type.constant("FLAGVALUES") %}
+          {{perm.id}}
+          {% end %}
+        {% end %}
+      end
+    end
+
+    macro build_consts
+      {% if @type.has_constant?("FLAGVALUES") %}
+        {% for perm in @type.constant("FLAGVALUES") %}
+          {{perm.id}} = {{@type}}.new(PermValues::{{perm.id}}.value.to_u32)
+        {% end %}
+      {% end %}
+      None = {{@type}}.new(PermValues::None.value.to_u32)
+    end
+
+    macro build_permission_requests
+      {% if @type.has_constant?("FLAGVALUES") %}
+        {% for perm in @type.constant("FLAGVALUES") %}
+          def permits_{{perm.id.underscore}}?
+            return @value & PermValues::{{perm.id}}.value == PermValues::{{perm.id}}.value
+          end
+        {% end %}
+      {% end %}
+      def permits_something?
+        return @value != 0
+      end
+    end
+
+    # the macro inherited is called right after that the class declaration has been made
+    # so the class has no constant yet.
+    # Instead, we put our code inside the finished macro to make sure that the class is fully defined.
+    macro inherited
+      macro finished
+        # use verbatim to avoid macro escaping
+        {% verbatim do %}
+          {% if @type.annotation(Berm::FlagsConstant) == nil %}
+            {% raise "Class variable FlagValues not found" if !@type.has_constant?("FlagValues") %}
+            FLAGVALUES = {{FlagValues}}
+          {% else %}
+            {% raise "Class variable " + @type.annotation(Berm::FlagsConstant)[0] + " not found" if !@type.constant(@type.annotation(Berm::FlagsConstant)[0]) %}
+            FLAGVALUES = {{@type.constant(@type.annotation(Berm::FlagsConstant)[0])}}
+          {% end %}
+
+          init_values
+          build_consts
+
+          getter value
+
+          def initialize(@value : UInt32)
+          end
+
+          def |(other : {{@type}})
+            {{@type}}.new(value | other.value)
+          end
+
+          def &(other : {{@type}})
+            {{@type}}.new(value & other.value)
+          end
+
+          def to_s(io : IO)
+            enabled = [] of PermValues
+            PermValues.each do |val|
+              enabled << val if value & val.value == val.value
+            end
+            enabled << PermValues::None if enabled.size == 0
+            io << "{{@type}}::#{enabled.join("|")}"
+          end
+
+          build_permission_requests
+        {% end %}
+      end
+    end
+  end
+end
